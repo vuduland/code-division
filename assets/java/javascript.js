34490f023bf7f06ceb204ef91ac8a7bdb5db93d3
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-10-23 09:48:15 -0500

    add randNum generator; combined K's css with mine

--- assets/java/javascript.js
+++ assets/java/javascript.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-	q = "computer"; // search query
+  q = "computer"; // search query
 
-	request = new XMLHttpRequest();
-	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q+'&rid=250w', true);
+  request = new XMLHttpRequest();
+  request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + q + '&rid=250w', true);
 
-	request.onload = function() {
-		if (request.status >= 200 && request.status < 400){
-			data1 = JSON.parse(request.responseText).data.image_url;
-			console.log(data1);
-            document.getElementById("gifComputer").innerHTML = '<center><img src = "'+data1+'"  title="GIF via Giphy" style= "width: 100%"  ></center>';
-
-
-		} else {
-			console.log('reached giphy, but API returned an error');
-		 }
-	};
-
-	request.onerror = function() {
-		console.log('connection error');
-	};
-
-	request.send();
-});
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      data1 = JSON.parse(request.responseText).data.image_url;
+      console.log(data1);
+      document.getElementById("gifComputer").innerHTML = '<center><img src = "' + data1 + '"  title="GIF via Giphy" style= "width: 100%"  ></center>';
 
 
-function loadArticle() {
+    } else {
+      console.log('reached giphy, but API returned an error');
+    }
+  };
+
+  request.onerror = function () {
+    console.log('connection error');
+  };
+
+  request.send();
+},
+
+
+function loadArticle() { 
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coding&api-key=iXxrB5IFdP1u6aNOnFTRt1J9L3jYnjBJ";
     $.ajax({
         url: queryURL,
@@ -41,9 +41,8 @@ function loadArticle() {
 
     });
 }
-
-
-$(document).ready(loadArticle);
+  loadArticle();
+// $(document).ready(loadArticle);
 
 
 
--- assets/java/new-javascript.js
+++ assets/java/new-javascript.js
@@ -1,86 +1,104 @@
-// on page load, search for & display a random gif matching your search term using the Giphy API.
-// usage: 
-
-document.addEventListener('DOMContentLoaded', function () {
-	q = "computer"; // search query
-	
-	request = new XMLHttpRequest();
-	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q+'&rid=250w', true);
-	
-	request.onload = function() {
-		if (request.status >= 200 && request.status < 400){
-			data1 = JSON.parse(request.responseText).data.image_url;
-			console.log(data1);
-            document.getElementById("gifComputer").innerHTML = '<center><img src = "'+data1+'"  title="GIF via Giphy" style= "width: 100%"  ></center>';
-            
-
-		} else {
-			console.log('reached giphy, but API returned an error');
-		 }
-	};
-
-	request.onerror = function() {
-		console.log('connection error');
-	};
-
-	request.send();
-});
-
 //===========================================================================================//
 
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=web+development&api-key=iXxrB5IFdP1u6aNOnFTRt1J9L3jYnjBJ";
 function loadArticle() {
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coding&api-key=iXxrB5IFdP1u6aNOnFTRt1J9L3jYnjBJ";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (results) {
-        // console.log(results)
-            
-      var article = results.response.docs;
-      console.log(results.response.docs);
-        for (var i = 0; i < article.length; i++) {
-            console.log(i);
-            console.log(article);
-            console.log(article[i].web_url);
-        }
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (results) {
+    var a = results;
+    console.log(results);
+    var article = { a: "response"};
+    var headlineArr = [];
+    var headline = "";
+    var randomNum = [];
+    for (var i = 0; i < 3; i++) {
+      // console.log(i);
+      // console.log(article);
+      //tried my best to make sure of no duplicates, although it's still possible.
+      randomNum[i] = Math.round(Math.random() * 10);
+      if (randomNum[i] !== randomNum[++i] && randomNum[i] !== randomNum[--i]) {
+         headlineArr[i] = article[randomNum[i]].headline.main;
+      } else if (randomNum[i] === randomNum[--i]) {
+        randomNum[i] = Math.round((Math.random * 10) % 2);
+        headlineArr[i] = article[randomNum[i]];
+      }
 
-    });
+      //generating the divs
+      document.createElement('article');
+
+
+      console.log(randomNum);
+     
+      console.log(headlineArr[i] + " = headline");
+
+    }
+  });
 }
-    
-
-$(document).ready(loadArticle);
+loadArticle();
+// loadArticle();
+// $(document).ready();
 
 //===========================================================================================//
 
-const myInitCallback = function() {
-    if (document.readyState == 'complete') {
-      // Document is ready when CSE element is initialized.
+const myInitCallback = function () {
+  if (document.readyState == 'complete') {
+    // Document is ready when CSE element is initialized.
+    // Render an element with both search box and search results in div with id 'test'.
+    google.search.cse.element.render({
+      div: "customSearch",
+      tag: 'search'
+    });
+  } else {
+    // Document is not ready yet, when CSE element is initialized.
+    google.setOnLoadCallback(function () {
       // Render an element with both search box and search results in div with id 'test'.
-      google.search.cse.element.render(
-          {
-            div: "customSearch",
-            tag: 'search'
-           });
-    } else {
-      // Document is not ready yet, when CSE element is initialized.
-      google.setOnLoadCallback(function() {
-         // Render an element with both search box and search results in div with id 'test'.
-          google.search.cse.element.render(
-              {
-                div: "customSearch",
-                tag: 'search'
-              });
-      }, true);
-    }
-  };
-  
-  // Insert it before the CSE code snippet so the global properties like parsetags and callback
-  // are available when cse.js runs.
+      google.search.cse.element.render({
+        div: "customSearch",
+        tag: 'search'
+      });
+    }, true);
+  }
+};
 
-  // initializes Google Custom Search Call Back
-  window.__gcse = {
-    parsetags: 'explicit',
-    initializationCallback: myInitCallback
+// Insert it before the CSE code snippet so the global properties like parsetags and callback
+// are available when cse.js runs.
+
+// initializes Google Custom Search Call Back
+window.__gcse = {
+  parsetags: 'explicit',
+  initializationCallback: myInitCallback
 };
 
 
+//===========================================================================================//
+
+
+
+// on page load, search for & display a random gif matching your search term using the Giphy API.
+// usage: 
+document.addEventListener('DOMContentLoaded', function () {
+  q = "computer"; // search query
+
+  request = new XMLHttpRequest();
+  request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + q + '&rid=250w', true);
+
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      data1 = JSON.parse(request.responseText).data.image_url;
+      console.log(data1);
+      document.getElementById("gifComputer").innerHTML = '<center><img src = "' + data1 + '"  title="GIF via Giphy" style= "width: 100%"  ></center>';
+
+
+    } else {
+      console.log('reached giphy, but API returned an error');
+    }
+  };
+
+  request.onerror = function () {
+    console.log('connection error');
+  };
+
+  request.send();
+});
