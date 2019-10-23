ff343a25511f8260c600e950b0f6b08a50ce439a
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-10-23 06:19:27 -0500

    added kregg index, style, style, javascript.js

--- assets/java/javascript.js
+++ assets/java/javascript.js
@@ -1,18 +1,15 @@
-// on page load, search for & display a random gif matching your search term using the Giphy API.
-// usage: 
-
 document.addEventListener('DOMContentLoaded', function () {
 	q = "computer"; // search query
-	
+
 	request = new XMLHttpRequest();
 	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q+'&rid=250w', true);
-	
+
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
 			data1 = JSON.parse(request.responseText).data.image_url;
 			console.log(data1);
             document.getElementById("gifComputer").innerHTML = '<center><img src = "'+data1+'"  title="GIF via Giphy" style= "width: 100%"  ></center>';
-            
+
 
 		} else {
 			console.log('reached giphy, but API returned an error');
@@ -26,7 +23,6 @@ document.addEventListener('DOMContentLoaded', function () {
 	request.send();
 });
 
-//===========================================================================================//
 
 function loadArticle() {
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coding&api-key=iXxrB5IFdP1u6aNOnFTRt1J9L3jYnjBJ";
@@ -35,9 +31,8 @@ function loadArticle() {
         method: "GET"
     }).then(function (results) {
         // console.log(results)
-            
-      var article = results.response.docs;
-      console.log(results.response.docs);
+
+        var article = results.response.docs;
         for (var i = 0; i < article.length; i++) {
             console.log(i);
             console.log(article);
@@ -46,11 +41,13 @@ function loadArticle() {
 
     });
 }
-    
+
 
 $(document).ready(loadArticle);
 
-//===========================================================================================//
+
+
+
 
 const myInitCallback = function() {
     if (document.readyState == 'complete') {
@@ -73,7 +70,7 @@ const myInitCallback = function() {
       }, true);
     }
   };
-  
+
   // Insert it before the CSE code snippet so the global properties like parsetags and callback
   // are available when cse.js runs.
 
@@ -82,5 +79,3 @@ const myInitCallback = function() {
     parsetags: 'explicit',
     initializationCallback: myInitCallback
 };
-
-
